000f5da010f38ec1e9d3b0e0a289f0ad19af2843
dell <dell@Kingmaker>
2017-06-25 01:15:19 +0530

    my first commit

---
+++ testProj/src/com/test/main/HelloJava.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.test.main;
+
+/**
+ * @author dell
+ *
+ */
+public class HelloJava {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
